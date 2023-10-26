5fb7dd88b8cfe43cb9ecc85d3079d31c3367acba
Pr3Dat3UR_23 <120458546+Pr3Dat3UR23@users.noreply.github.com>
2023-10-26 17:50:57 +0200

    Ajout monnaie class_PlayerHand.py
    parce que apres gngngngn c'est qui le branleur maintenant ?

--- class_PlayerHand.py
+++ class_PlayerHand.py
@@ -1,13 +1,20 @@
 class PlayerHand:
-    def __init__(self):
+    def __init__(self, monnaie):
         self.cartes = []
+        self.monnaie = monnaie
 
 #Getters
 
     def get_cartes(self):
         return self.cartes
+    
+    def get_monnaie(self):
+        return self.monnaie
 
 #Setters
 
     def add_cartes(self,card):
         self.cartes.append(card)
+    
+    def set_monnaie(self,new_moula):
+        self.monnaie = new_moula
