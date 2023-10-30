0d8cba75bc925522123d100068d0748ad7c3a126
Pr3Dat3UR_23 <120458546+Pr3Dat3UR23@users.noreply.github.com>
2023-10-30 20:40:55 +0100

    Update game.py
    Ajout de Texte affichant l'argent de chaque joueurs
    Création d'Images contenant le texte grâce à une variable (Pour chaque Joueurs bien évidement)
    Placements des "images" a coté des joueurs correspondants

--- game.py
+++ game.py
@@ -54,11 +54,46 @@ def game(screen):
                 if phase<4:
                     phase=phase+1
 
+
         screen.blit(background_image, (0, 0))
         screen.blit(enemycard, (700, 10))
         screen.blit(enemycard, (700, 60))
         screen.blit(pygame.transform.rotate(enemycard,90), (-150, 60))
         screen.blit(pygame.transform.rotate(enemycard,90), (-150, 10))
+
+        #Affichage differents textes (scores)
+
+            #Mise en place des variables
+
+        police = pygame.font.SysFont("monospace",20)
+
+                #Pour le Joueur
+
+        player_monnaie = str(player_hand.affiche_total_monnaie())
+        texte_player = police.render ("Argent Joueur :", 1 ,(150,0,0)) #Le (150,0,0) est la couleur du texte en RGB, pareil pour les suivants
+        player_texte = police.render (player_monnaie,1,(150,0,0))
+
+                #Pour l'adversaire 1
+        adversaire1_monnaie = str(adversaire1_hand.affiche_total_monnaie())
+        texte_adversaire1 = police.render ("Argent Adversaire :", 1 ,(150,0,0))
+        adversaire1_texte = police.render (adversaire1_monnaie, 1 ,(150,0,0))
+
+                #Pour l'adversaire 2
+
+        adversaire2_monnaie = str(adversaire2_hand.affiche_total_monnaie())
+        texte_adversaire2 = police.render ("Argent Adversaire :", 1 ,(150,0,0))
+        adversaire2_texte = police.render (adversaire2_monnaie, 1 ,(150,0,0))
+
+
+            # Placement
+
+        screen.blit(texte_player,(25,500))
+        screen.blit(player_texte,(50,525))
+        screen.blit(texte_adversaire1,(25,300))
+        screen.blit(adversaire1_texte,(50,325))
+        screen.blit(texte_adversaire2,(550,300))
+        screen.blit(adversaire2_texte,(700,325))
+
         played_card(player_hand, screen, pl[0], 0)
         played_card(player_hand, screen, pl[1], 1)
         adversaire_card(adversaire1_hand, en1[0])
