0b4d14f5298de872d4688810c55957a088447f25
Pr3Dat3UR_23 <120458546+Pr3Dat3UR23@users.noreply.github.com>
2023-10-30 12:19:04 +0100

    Update class_EnemyHand.py
    Meme chose que PlayerHand

--- class_EnemyHand.py
+++ class_EnemyHand.py
@@ -1,7 +1,8 @@
-from random import *
+from random import*
 class EnemyHand:
-    def __init__(self, monnaie = randint(100,500)):
+    def __init__(self, monnaie = [randint(3, 15),randint(2, 12),randint(1, 8),randint(0, 5),randint(0, 3),randint(0, 10)]):
         self.cartes = []
+        self.monnaie = monnaie
 
 #Getters
 
@@ -15,6 +16,11 @@ class EnemyHand:
 
     def add_cartes(self,card):
         self.cartes.append(card)
-
+    
     def set_monnaie(self,new_moula):
         self.monnaie = new_moula
+
+#Autres
+
+    def affiche_total_monnaie(self):
+        return self.monnaie[0]*25 + self.monnaie[1]*50 + self.monnaie[2]*100 + self.monnaie[3]*200 + self.monnaie[4]*500 + self.monnaie[5]*1000
